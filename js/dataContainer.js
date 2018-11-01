700b49bc9555b01af5f37370819687391a307b4b
david940918 <david940918@gmail.com>
2018-11-02 02:06:31 +0900

    시간표 생성을 위한 함수 및 스크립트 추가 + package json파일 추가

---
+++ js/dataContainer.js
@@ -0,0 +1,68 @@
+function Lecture(name, time, prof, num){
+  this.name = name;
+  this.time = time;
+  this.prof = prof;
+  this.num = num;
+}
+// 기초 교양 : 39개
+let lecture = [
+  new Lecture("강의 1", "mon_09_11/wed_10_11", "Jang", "1000"),
+  new Lecture("강의 2", "tue_10_12/thu_12_13", "Jang", "1001"),
+  new Lecture("강의 3", "mon_14_15/wed_14_16", "Jang", "1002"),
+  new Lecture("강의 4", "tue_13_16", "Jang", "1003"),
+  new Lecture("강의 5", "fri_09_12", "Jang", "1004"),
+  new Lecture("강의 6", "mon_09_11/wed_10_11", "Jang", "1000"),
+  new Lecture("강의 7", "tue_10_12/thu_12_13", "Jang", "1001"),
+  new Lecture("강의 8", "mon_14_15/wed_14_16", "Jang", "1002"),
+  new Lecture("강의 9", "tue_13_16", "Jang", "1003"),
+  new Lecture("강의 10", "fri_09_12", "Jang", "1004"),
+  new Lecture("강의 11", "mon_09_11/wed_10_11", "Jang", "1000"),
+  new Lecture("강의 12", "tue_10_12/thu_12_13", "Jang", "1001"),
+  new Lecture("강의 13", "mon_14_15/wed_14_16", "Jang", "1002"),
+  new Lecture("강의 14", "tue_13_16", "Jang", "1003"),
+  new Lecture("강의 15", "fri_09_12", "Jang", "1004"),
+  new Lecture("강의 16", "mon_09_11/wed_10_11", "Jang", "1000"),
+  new Lecture("강의 17", "tue_10_12/thu_12_13", "Jang", "1001"),
+  new Lecture("강의 18", "mon_14_15/wed_14_16", "Jang", "1002"),
+  new Lecture("강의 19", "tue_13_16", "Jang", "1003"),
+  new Lecture("강의 20", "fri_09_12", "Jang", "1004"),
+  new Lecture("강의 21", "mon_09_11/wed_10_11", "Jang", "1000"),
+  new Lecture("강의 22", "tue_10_12/thu_12_13", "Jang", "1001"),
+  new Lecture("강의 23", "mon_14_15/wed_14_16", "Jang", "1002"),
+  new Lecture("강의 24", "tue_13_16", "Jang", "1003"),
+  new Lecture("강의 25", "fri_09_12", "Jang", "1004"),
+  new Lecture("강의 26", "mon_09_11/wed_10_11", "Jang", "1000"),
+  new Lecture("강의 27", "tue_10_12/thu_12_13", "Jang", "1001"),
+  new Lecture("강의 28", "mon_14_15/wed_14_16", "Jang", "1002"),
+  new Lecture("강의 29", "tue_13_16", "Jang", "1003"),
+  new Lecture("강의 30", "fri_09_12", "Jang", "1004"),
+  new Lecture("강의 31", "mon_09_11/wed_10_11", "Jang", "1000"),
+  new Lecture("강의 32", "tue_10_12/thu_12_13", "Jang", "1001"),
+  new Lecture("강의 33", "mon_14_15/wed_14_16", "Jang", "1002"),
+  new Lecture("강의 34", "tue_13_16", "Jang", "1003"),
+  new Lecture("강의 35", "fri_09_12", "Jang", "1004"),
+  new Lecture("강의 36", "mon_09_11/wed_10_11", "Jang", "1000"),
+  new Lecture("강의 37", "tue_10_12/thu_12_13", "Jang", "1001"),
+  new Lecture("강의 38", "mon_14_15/wed_14_16", "Jang", "1002"),
+  new Lecture("강의 39", "tue_13_16", "Jang", "1003"),
+]
+
+let optionFunc = [
+  function(){alert("Mon");},
+  function(){alert("Tue");},
+  function(){alert("Wed");},
+  function(){alert("Thu");},
+  function(){alert("Fri");},
+  function(){alert("점심시간");},
+  function(){alert("9시 이후");},
+  function(){alert("10시 이후");},
+  function(){alert("11시 이후");},
+  function(){alert("12시 이후");},
+  function(){alert("15시 이후를 원해");},
+  function(){alert("16시 이후를 원해");},
+  function(){alert("17시 이후를 원해");},
+  function(){alert("18시 이후를 원해");},
+  function(){alert("Jang 교수님");},
+  function(){alert("Yong 교수님");},
+  function(){alert("Han 교수님");},
+]
